e349f031bc0b3c8fe7caa8efe61e460cee14cebd
mrs.leonenko <mrs.leonenko@gmail.com>
2018-04-01 17:03:07 -0400

    strange adventures of the GIT head

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import {MatM} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import {MatToolbarModule} from '@angular/material';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { TestComponent } from './test/test.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    TestComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Dish } from '../shared/dish';
+
+//import { DISHES } from '../shared/dishes';
+
+import { DishService } from '../services/dish.service';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  dishes: Dish[];
+
+  selectedDish: Dish; // = DISHES[0];
+  
+  constructor( private dishService: DishService) { }
+
+  ngOnInit() {
+    this.dishes = this.dishService.getDishes();
+  }
+
+  onSelect(dish: Dish) {
+    this.selectedDish = dish;
+  }
+
+}
