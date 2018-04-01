378ff963f5723b61cdf2844de371aa50fa476669
mrs.leonenko <mrs.leonenko@gmail.com>
2018-04-01 16:38:10 -0400

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,13 +18,18 @@ import { MenuComponent } from './menu/menu.component';
 import { TestComponent } from './test/test.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     TestComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +41,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
